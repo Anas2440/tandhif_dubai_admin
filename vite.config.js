e8945396ed2da2d6144be2262bf8149342dd8c4c
sakirraja00 <117822764+sakirraja00@users.noreply.github.com>
2025-08-09 16:00:22 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tandhif_Dubai/",
-  server: {
-    port: 2024,
-  },
-});
+  base: '/tandhif_Dubai/', // yeh tumhara live sub-path hai
+})
+  
+
